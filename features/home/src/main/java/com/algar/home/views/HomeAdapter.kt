9f60147979ab59be16b9c6de976f190205607d42
Daniell Algar <daniell.algar@gmail.com>
2019-11-22 13:59:39 +0100

    Revert "Support 3 different types in recycler view"
    This reverts commit e9245f54be62c4458c818bb6afeec221ec6da780.

--- features/home/src/main/java/com/algar/home/views/HomeAdapter.kt
+++ features/home/src/main/java/com/algar/home/views/HomeAdapter.kt
@@ -6,48 +6,22 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.algar.home.HomeViewModel
 import com.algar.home.R
-import com.algar.home.views.ItemType.FOOTER
-import com.algar.home.views.ItemType.HEADER
-import com.algar.home.views.ItemType.MIDDLE
 import com.algar.model.Article
 import com.algar.model.NewsResponse
 import com.algar.remote.model.ApiResponse
-
-enum class ItemType(val value: Int) {
-    HEADER(0),
-    MIDDLE(1),
-    FOOTER(2)
-}
 
 class HomeAdapter(private val viewModel: HomeViewModel): RecyclerView.Adapter<HomeViewHolder>() {
 
     private val articles: MutableList<Article> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        val layout = when (viewType) {
-            HEADER.value -> R.layout.item_home_header
-            FOOTER.value -> R.layout.item_home_footer
-            else -> R.layout.item_home_middle
-        }
-        return HomeViewHolder(LayoutInflater.from(parent.context).inflate(layout, parent, false))
+        return HomeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_home, parent, false))
     }
 
     override fun getItemCount(): Int = articles.size
 
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        holder.bindTo(article = articles[position], viewType = getItemViewType(position))
-    }
-
-    override fun getItemViewType(position: Int): Int = when (position) {
-        0 -> {
-            HEADER.value
-        }
-        articles.size - 1 -> {
-            FOOTER.value
-        }
-        else -> {
-            MIDDLE.value
-        }
+        holder.bindTo(article = articles[position])
     }
 
     // TODO: This is planned to receive a Resource in the future. Keeping it like this for now.
--- features/home/src/main/java/com/algar/home/views/HomeViewHolder.kt
+++ features/home/src/main/java/com/algar/home/views/HomeViewHolder.kt
@@ -2,27 +2,14 @@ package com.algar.home.views
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.algar.home.databinding.ItemHomeMiddleBinding
-import com.algar.home.databinding.ItemHomeFooterBinding
-import com.algar.home.databinding.ItemHomeHeaderBinding
-import com.algar.home.views.ItemType.FOOTER
-import com.algar.home.views.ItemType.HEADER
+import com.algar.home.databinding.ItemHomeBinding
 import com.algar.model.Article
 
-class HomeViewHolder(private val parent: View): RecyclerView.ViewHolder(parent) {
+class HomeViewHolder(parent: View): RecyclerView.ViewHolder(parent) {
 
-    fun bindTo(article: Article, viewType: Int) = when (viewType) {
-        HEADER.value -> {
-            val bindingHeader = ItemHomeHeaderBinding.bind(parent)
-            bindingHeader.article = article
-        }
-        FOOTER.value -> {
-            val bindingFooter = ItemHomeFooterBinding.bind(parent)
-            bindingFooter.article = article
-        }
-        else -> {
-            val bindingMiddle = ItemHomeMiddleBinding.bind(parent)
-            bindingMiddle.article = article
-        }
+    private val binding = ItemHomeBinding.bind(parent)
+
+    fun bindTo(article: Article) {
+        binding.article = article
     }
 }
